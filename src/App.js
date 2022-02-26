8c6ef5a0be89c2f6a43552e8e283f7b9b17caf8e
xocignacio <xose_ignacio@hotmail.com>
2022-02-26 16:21:59 -0300

    first repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola, bienvenidos a mi proyectoReact
         </p>
         <a
           className="App-link"
